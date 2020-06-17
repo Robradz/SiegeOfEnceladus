d9887e43466d9f07f78b339550b98195ca7311ac
unknown <rradzvil@ucsc.edu>
2020-06-16 18:10:49 -0700

    Splash Screen and Level 1

---
+++ Assets/SplashScreenScript.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreenScript : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadLevel", 2f);
+    }
+
+    void LoadLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
